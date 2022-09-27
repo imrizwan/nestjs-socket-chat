d88239a354e7cc8509421bd856f9a2815b9212e8
Rizwan <rizwan@tncitgroup.com>
2022-09-27 15:51:30 +0400

    chat broadcast completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,27 +2,25 @@ import { useState, useEffect } from "react";
 import moment from "moment";
 import io from 'socket.io-client';
 import './App.css';
-const socket = io('http://localhost:4000');
+const socket = io('http://172.16.25.31:4000');
 function App() {
 
-  const senderColors = ["#e74c3c", "#8e44ad", "#3498db", "#2ecc71"];
-  const receiverColors = ["#c0392b", "#7d3c98", "#2980b9", "#27ae60"];
+  const colors = ["#e74c3c", "#8e44ad", "#3498db", "#2ecc71", "#c0392b", "#7d3c98", "#2980b9", "#27ae60"];
 
-  const [fields, setFields] = useState({ messages: [], message: '', name: '', nameDisabled: false, senderColor: senderColors[Math.floor(Math.random() * senderColors.length)], receiverColor: receiverColors[Math.floor(Math.random() * receiverColors.length)] });
+  const [fields, setFields] = useState({ messages: [], message: '', name: '', nameDisabled: false, color: '' });
   const [isConnected, setIsConnected] = useState(socket.connected);
-  const [lastPong, setLastPong] = useState(null);
 
   useEffect(() => {
     let name = localStorage.getItem('name');
+    let color = localStorage.getItem('color');
     let isDisabled = name ? true : false;
     name = name ? name : '';
-    setFields({ ...fields, nameDisabled: isDisabled, name });
+    color = color ? color : '';
+    setFields({ ...fields, nameDisabled: isDisabled, name, color });
+    // eslint-disable-next-line
   }, []);
 
   useEffect(() => {
-    socket.io.on("connection", (error) => {
-      // ...
-    });
     socket.on('connect', () => {
       console.log('Connected');
       setIsConnected(true);
@@ -34,7 +32,8 @@ function App() {
 
     socket.on("message", async (arg1) => {
       try {
-        console.log("listening", arg1); // 1
+        let temp = [arg1, ...fields.messages];
+        setFields({ ...fields, messages: temp, message: '' });
       } catch (e) {
         console.log(e);
       }
@@ -47,7 +46,7 @@ function App() {
 
   const sendMessage = (msg) => {
     try {
-      socket.emit('message', msg, (data) => console.log(data));
+      socket.emit('message', msg, (data) => { console.log(data) });
     } catch (e) {
       console.log(e);
     }
@@ -56,7 +55,10 @@ function App() {
   return (
     <div className="App">
       <div className="center">
-        <input
+        <div>
+          <p>Status: {isConnected ? 'Connected': 'Disconnected'}</p>
+        </div>
+        Name: <input
           disabled={fields.nameDisabled}
           placeholder="Your message"
           value={fields.name}
@@ -69,9 +71,11 @@ function App() {
               if (fields.name.length < 3) {
                 alert('Name must be at least 3 characters long');
               } else {
+                let color = colors[Math.floor(Math.random() * colors.length)];
                 localStorage.setItem('name', fields.name);
+                localStorage.setItem('color', color);
                 localStorage.setItem('nameDisabled', true);
-                setFields({ ...fields, nameDisabled: true });
+                setFields({ ...fields, nameDisabled: true, color });
               }
             }
           }} />
@@ -79,10 +83,10 @@ function App() {
       <div className="container">
         <div className="chat-container">
           {fields.messages.map((msg, index) => (
-            <div className={msg.type === 'sent' ? "message sender" : "message receiver"} key={index}>
-              <div id="container" style={{ background: msg.type === 'sent' ? fields.senderColor : fields.receiverColor }}>
+            <div className={msg.name === fields.name ? "message receiver" : "message sender"} key={index}>
+              <div id="container" style={{ background: msg.color }}>
                 <div id="name">
-                  {fields.name ? fields.name.split('').slice(0, 1) : ''}
+                  {msg.name ? msg.name.split('').slice(0, 1) : ''}
                 </div>
               </div>
               {/* <img className="avatar" alt="profile" src="https://placeimg.com/50/50/people?1" /> */}
@@ -94,6 +98,7 @@ function App() {
         <div className="send-message-form">
 
           <input
+            disabled={!isConnected}
             placeholder="Your message"
             value={fields.message}
             type="text"
@@ -105,22 +110,17 @@ function App() {
                 if (!fields.name) {
                   return alert("Please enter your name");
                 } else {
-                  let msg = { type: 'sent', message: fields.message, datetime: new Date() };
+                  let msg = { type: 'sent', message: fields.message, datetime: new Date(), name: fields.name, color: fields.color };
+                  console.log("msg", msg)
                   let temp = [msg, ...fields.messages];
-                  sendMessage(msg);
                   setFields({ ...fields, messages: temp, message: '' });
+                  sendMessage(msg);
                 }
               }
             }} />
           <button type="submit">Send</button>
         </div>
       </div>
-
-      {/* <div>
-        <p>Connected: {'' + isConnected}</p>
-        <p>Last pong: {lastPong || '-'}</p>
-        <button onClick={sendMessage}>Send ping</button>
-      </div> */}
     </div>
   );
 }
--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -12,18 +12,23 @@ import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
     cors: {
-        origin: 'http://localhost:3000',
-    },
+        origin: 'http://172.16.25.31:3000',
+    }
 })
 
 export class EventsGateway {
     @WebSocketServer()
     server: Server;
 
+    // @SubscribeMessage('message')
+    // findAll(@MessageBody() data: string, @ConnectedSocket() socket: Socket): Observable<WsResponse<number>> {
+    //     socket.emit('message', data);
+    //     return from([1,2,3]).pipe(map(item => ({ event: 'message', data: item })));
+    // }
+
     @SubscribeMessage('message')
-    findAll(@MessageBody() data: string, @ConnectedSocket() socket: Socket): Observable<WsResponse<number>> {
-        socket.join("chat");
-        socket.emit('message', data);
-        return from([1,2,3]).pipe(map(item => ({ event: 'message', data: item })));
+    async identity(@MessageBody() data: number, @ConnectedSocket() socket: Socket): Promise<number> {
+        socket.broadcast.emit('message', data);
+        return data;
     }
 }
